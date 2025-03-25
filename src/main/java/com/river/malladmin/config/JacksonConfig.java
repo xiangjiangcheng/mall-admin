0a42a97c2cb69e9ab676c4b903998d927443a925
JiangCheng Xiang <Jiangcheng.Xiang@FreyrSolutions.com>
2025-03-25 15:35:46 +0800

    feat: 添加LocalDateTime全局序列化

---
+++ src/main/java/com/river/malladmin/config/JacksonConfig.java
@@ -0,0 +1,25 @@
+package com.river.malladmin.config;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public JavaTimeModule javaTimeModule() {
+        JavaTimeModule module = new JavaTimeModule();
+        // 设置 LocalDateTime 的序列化和反序列化格式
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
+        return module;
+    }
+
+}
